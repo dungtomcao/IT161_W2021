18b034870afa1cb9fc01bc2a684e876cf27342c3
Tom <tomcao090@gmail.com>
2023-01-31 19:22:17 +0700

    New footer
    New footer uses php to display appropriate website name

--- fp/includes/footer.php
+++ fp/includes/footer.php
@@ -1,5 +1,7 @@
 <footer>
-      <p><small>&copy; 2022 by <a href="contact.php">Tom Cao</a>, All Rights Reserved ~ <a href="https://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+      <p><small>&copy; 2022 by <a href="contact.php">Tom Cao</a>, All Rights Reserved ~ <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fweb-students.net%2Fsite8%2Fit161_W21%2Ffp%2F<?php
+echo htmlspecialchars(basename($_SERVER['PHP_SELF']));?>" target="_blank">Valid HTML</a> ~ <a href="https://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fweb-students.net%2Fsite8%2Fit161_W21%2Ffp%2F<?php
+echo htmlspecialchars(basename($_SERVER['PHP_SELF']));?>" target="_blank">Valid CSS</a></small></p>
     </footer>
 
 </body>
